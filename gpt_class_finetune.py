4e5f93c6e0c12d76e3fa6e8cc523457b82b88f2e
fjjjjjf <853723049@qq.com>
2025-11-13 11:21:23 +0800

    add download gpt weight code

--- gpt_class_finetune.py
+++ gpt_class_finetune.py
@@ -5,6 +5,8 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset
 from pathlib import Path
+from torch.utils.data import DataLoader
+import tiktoken
 
 url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
 zip_path = "sms_spam_collection.zip"
@@ -88,16 +90,101 @@ def random_split(df,train_frac,validation_frac):
     return train_df,validation_df,test_df  #训练，验证，测试集
 
 
+
+
+    return settings, params
 if __name__ =='__main__':
     #download_and_unzip_spam_data(url,zip_path,extracted_path,data_file_path)
+    
     df = pd.read_csv(data_file_path,sep='\t',header= None,names=['Label','Text'])
-    print(df['Label'].value_counts())
     balanced_df = create_balanced_dataset(df)
-    print(balanced_df['Label'].value_counts())
+    
+    #print(df['Label'].value_counts())
+    #print(balanced_df['Label'].value_counts())
 
     balanced_df['Label'] =balanced_df['Label'].map({"ham":0,"spam":1})  #转换token
 
-    train_df,validation_df,test_df= random_split(balanced_df,0.7,0.1)  #分割保存数据
-    train_df.to_csv("train.csv",index=None)
-    validation_df.to_csv("validation.csv",index=None)
-    test_df.to_csv("test.csv",index=None)
+    # train_df,validation_df,test_df= random_split(balanced_df,0.7,0.1)  #分割保存数据
+    # train_df.to_csv("train.csv",index=None)
+    # validation_df.to_csv("validation.csv",index=None)
+    # test_df.to_csv("test.csv",index=None)
+
+    tokenizer = tiktoken.get_encoding('gpt2')
+
+    train_dataset = SpamDataset(
+        csv_file= 'train.csv',
+        tokenizer= tokenizer,
+        max_len= None,
+    )
+
+    val_dataset =SpamDataset(
+        csv_file='validation.csv',
+        tokenizer= tokenizer,
+        max_len= train_dataset.max_length
+    )
+
+    test_dataset =SpamDataset(
+        csv_file='test.csv',
+        tokenizer= tokenizer,
+        max_len= train_dataset.max_length
+    )
+
+    num_workers =0
+    batch_size =8
+    torch.manual_seed(123)
+
+    train_loader =DataLoader(dataset= train_dataset,
+                             batch_size=batch_size,
+                             shuffle= True,
+                             num_workers=num_workers,
+                             drop_last= True
+                             )
+    val_loader =DataLoader(dataset= val_dataset,
+                             batch_size=batch_size,
+                             shuffle= True,
+                             num_workers=num_workers,
+                             drop_last= False
+                             )
+    test_loader =DataLoader(dataset= test_dataset,
+                             batch_size=batch_size,
+                             shuffle= True,
+                             num_workers=num_workers,
+                             drop_last= False
+                             )
+    
+    for input_batch,target_batch in train_loader:
+        pass
+    # print ("Input batch dimension: ",input_batch.shape)  #[8,120]
+    #print("Target batch dimension: ",target_batch.shape) #[8]
+
+
+    ########################################
+    #导入模型
+    ########################################
+
+    CHOOSE_MODEL = "gpt2-small (124M)"
+    INPUT_PROMPT = "Every effort moves"
+
+    BASE_CONFIG = {
+        "vocab_size": 50257,     # Vocabulary size
+        "context_length": 1024,  # Context length
+        "drop_rate": 0.0,        # Dropout rate
+        "qkv_bias": True         # Query-key-value bias
+    }
+
+    model_configs = {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    }
+
+    BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+    assert train_dataset.max_length <= BASE_CONFIG["context_length"], (
+        f"Dataset length {train_dataset.max_length} exceeds model's context "
+        f"length {BASE_CONFIG['context_length']}. Reinitialize data sets with "
+        f"`max_length={BASE_CONFIG['context_length']}`"
+    )
+
+
