e6c0c714ea82a76d1dc427c3c697c957604bc577
fjjjjjf <853723049@qq.com>
2025-11-17 22:35:28 +0800

    增加微调模型，添加数据集，等操作

--- gpt_instruction_finetuning.py
+++ gpt_instruction_finetuning.py
@@ -3,6 +3,14 @@ import os
 import urllib.request
 import torch
 from torch.utils.data import Dataset    
+from torch.utils.data import DataLoader
+import tiktoken
+from gpt_download import download_and_load_gpt2
+from gpt_generate import load_weights_into_gpt,GPTModel,text_to_token_ids,token_ids_to_text,generate,train_model_simple,calc_loss_loader
+import time
+from gpt_class_finetune import plot_values
+from tqdm import tqdm
+import re
 
 class InstructionDataset(Dataset):
     def __init__(self,data,tokenizer):
@@ -96,18 +104,18 @@ def custom_collate_fn(
 ):
     batch_max_length =max(len(item)+1 for item in batch)
     inputs_lst,targets_lst =[],[]
-    for item in batch:
+    for item in batch:  
         new_item = item.copy()
         new_item +=[pad_token_id]
         padded =new_item+ [pad_token_id]*(batch_max_length-len(new_item))
         inputs = torch.tensor(padded[:-1])
         targets =torch.tensor(padded[1:])
 
-        mask =targets == pad_token_id   #在targets中，将触底个意外地所有填充标志换成ignore_index，换成-100
-        indices =torch.nonzero(mask).squeeze()
-
+        mask =targets == pad_token_id   #在targets中，将除第一个以外地所有填充标志换成ignore_index，换成-100
+        indices =torch.nonzero(mask).squeeze()  #targets = [11, 12, 50256] => mask = [False, False, True]=>indices = [2]
+ 
         if indices.numel()>1:
-            targets[indices[1:]] =ignore_index
+            targets[indices[1:]] =ignore_index  #如果有多个50256,则除第一个的都替换成-100,替换成-100是因为计算交叉熵会忽略标签为-100的目标，减少损失
 
         if allowed_max_length is not None:
             inputs =inputs[:allowed_max_length]
@@ -135,3 +143,140 @@ if __name__ =='__main__':
     train_data = data[:train_portion]
     val_data =data[train_portion:train_portion+test_portion]
     test_data = data[train_portion+test_portion:]
+
+    device =torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    num_workers =0
+    batch_size =8
+
+    torch.manual_seed(123)
+    tokenizer = tiktoken.get_encoding('gpt2')
+
+    train_dataset = InstructionDataset(train_data,tokenizer)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        collate_fn=custom_collate_fn,
+        shuffle=True,
+        drop_last=True,
+        num_workers = num_workers
+    )
+
+    val_dataset = InstructionDataset(val_data,tokenizer)
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        collate_fn= custom_collate_fn,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers
+    )
+
+    test_dataset =InstructionDataset(test_data,tokenizer)
+    test_loader =DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        collate_fn= custom_collate_fn,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers
+    )
+
+
+    CHOOSE_MODEL = "gpt2-medium (355M)"
+
+    BASE_CONFIG = {
+        "vocab_size": 50257,     # Vocabulary size
+        "context_length": 1024,  # Context length
+        "drop_rate": 0.0,        # Dropout rate
+        "qkv_bias": True         # Query-key-value bias
+    }
+
+    model_configs = {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    }
+
+    BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+    model_size = CHOOSE_MODEL.split()[-1].strip("()")
+    settings,params = download_and_load_gpt2(model_size=model_size,models_dir='gpt2')
+    model = GPTModel(BASE_CONFIG)
+
+    load_weights_into_gpt(model,params)
+    model.eval() 
+    model.to(device)
+
+    # input_text = format_input(val_data[0])
+    # print(input_text,'1')
+    # token_ids =generate(model=model,
+    #                     idx = text_to_token_ids(input_text,tokenizer).to(device),
+    #                     max_new_tokens=50,
+    #                     context_size=BASE_CONFIG['context_length'],
+    #                     eos_id=50256)
+    # generated_text = token_ids_to_text(token_ids,tokenizer)
+    # response_text = generated_text[len(input_text):].strip()
+
+    # print(response_text)
+
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader,model,device,num_batches=5)
+        val_loss = calc_loss_loader(val_loader,model,device,num_batches=5)
+
+    print("Training loss:",train_loss)
+    print("Validation loss:",val_loss)
+
+    start_time = time.time()
+    torch.manual_seed(123)
+
+    optimizer = torch.optim.AdamW(model.parameters(),lr=0.0005,weight_decay=0.1)
+    num_epochs =2
+
+    train_losses ,val_losses,token_seen =train_model_simple(
+        model,train_loader,val_loader,optimizer,device,
+        num_epochs=num_epochs,eval_freq=5,eval_iter=5,
+        start_context=format_input(val_data[0]),tokenizer=tokenizer
+    )
+
+    end_time =time.time()
+    execution_time_minutes =(end_time-start_time)/60
+
+    print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+
+
+    # for entry in test_data[:3]:
+    #     input_text =format_input(entry)
+    #     token_ids = generate(model=model,idx =text_to_token_ids(input_text,tokenizer).to(device),
+    #                          max_new_tokens=256,
+    #                          context_size= BASE_CONFIG["context_length"],
+    #                          eos_id=50256)
+    #     generated_text = token_ids_to_text(token_ids,tokenizer)
+    #     response_text  =generated_text[len(input_text):].replace('###Response:','').strip()
+    #     print(input_text)
+    #     print(f"\nCorrect response:\n>> {entry['output']}")
+    #     print(f"\nModel response:\n>> {response_text.strip()}")
+    #     print('-----------------------------------------------')
+
+
+    for i,entry in tqdm(enumerate(test_data),total=len(test_data)):
+        input_text = format_input(entry)
+
+        token_ids = generate(model=model,idx =text_to_token_ids(input_text,tokenizer).to(device),
+                             max_new_tokens=256,
+                             context_size= BASE_CONFIG["context_length"],
+                             eos_id=50256)
+        generated_text = token_ids_to_text(token_ids,tokenizer)
+        response_text = generated_text[len(input_text):].replace("### Response",'').strip()
+        test_data[i]['model_response'] =response_text
+    with open('instruction-data-with-response.json','w') as file:
+        json.dump(test_data,file,indent=4) 
+    print(test_data[0])
+
+    file_name = f"{re.sub(r'[ ()]', '', CHOOSE_MODEL) }-sft-standalone.pth"
+    torch.save(model.state_dict(), file_name)
+    print(f"Model saved as {file_name}")
+
+    # epochs_tensor =torch.linspace(0,num_epochs,len(train_losses))
+    # plot_values(epochs_tensor,token_seen,train_losses,val_losses)
+    
